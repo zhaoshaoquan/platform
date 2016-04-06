5afe5c2f0402ea4a560e1e7094f3a8ae3d5b6db5
zhaoshaoquan <10534986@qq.com>
2016-04-06 18:11:29 +0800

    优化maven脚本

--- commons/install.bat
+++ commons/install.bat
@@ -1,4 +1,5 @@
 @echo off
+cd %~d0
 cd %~dp0
 call mvn clean install -Dmaven.test.skip=true
 pause
--- package.bat
+++ package.bat
@@ -1,4 +1,5 @@
 @echo off
+cd %~d0
 cd %~dp0
 call mvn clean compile package -Dmaven.test.skip=true
 pause
