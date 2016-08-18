3593e776d8e3e632d7bbccabfbcbede0d4a5448c
zhaoshaoquan <10534986@qq.com>
2016-08-18 22:41:41 +0800

    1、新增加mybatis中sql的日志配置 2、统一对各模块的版本进行配置和管理 3、删除extjs模块

--- extjs/deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-%~d0
-cd %~dp0
-call mvn clean install deploy -Dmaven.test.skip=true
-pause
--- extjs/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-%~d0
-cd %~dp0
-call mvn clean install -Dmaven.test.skip=true
-pause
