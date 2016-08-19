cfd57ec8e1cf511f0bcb524e8df88ded694ed277
zhaoshaoquan <10534986@qq.com>
2016-08-19 09:28:57 +0800

    修正GlobalConfig加载配置文件时，输出详情

--- commons/src/main/java/com/stone/commons/GlobalConfig.java
+++ commons/src/main/java/com/stone/commons/GlobalConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,20 +19,24 @@ import java.util.Properties;
  * 2016年2月6日 下午10:37:27
  */
 public class GlobalConfig{
-	private static final Logger log = LoggerFactory.getLogger(GlobalConfig.class);
+	private static final Logger logger = LoggerFactory.getLogger(GlobalConfig.class);
 	private static final Properties properties = new Properties();
 	static{
         try{
+            StringBuilder filePaths = new StringBuilder();
             ClassPathResource cpr = new ClassPathResource("config.properties");
+            Assert.notNull(cpr, "classes目录中缺少config.properties配置文件");
+            filePaths.append(cpr.getURI()).append("\n");
             properties.load(new InputStreamReader(cpr.getInputStream(), "UTF-8"));
             ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
             Resource[] resources = resourcePatternResolver.getResources("classpath*:META-INF/config/*.properties");
             for(Resource resource : resources){
+                filePaths.append(resource.getURI()).append("\n");
                 properties.load(new InputStreamReader(resource.getInputStream(), "UTF-8"));
             }
-            log.info("====配置文件已读取完成====");
+            logger.info(String.format("\n=====加载配置文件开始=====\n%s=====加载配置文件完成=====\n", filePaths.toString()));
         }catch(IOException e){
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
         }
 	}
 	
