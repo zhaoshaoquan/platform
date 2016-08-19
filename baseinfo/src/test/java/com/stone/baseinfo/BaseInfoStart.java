87017b4f1c6b0e1a5a6d385dc39c3f544e582039
zhaoshaoquan <10534986@qq.com>
2016-08-19 09:47:35 +0800

    修改jetty启动

--- baseinfo/src/test/java/com/stone/baseinfo/BaseInfoStart.java
+++ baseinfo/src/test/java/com/stone/baseinfo/BaseInfoStart.java
@@ -5,7 +5,7 @@ import static com.stone.jetty.JettyUtil.newJettyUtil;
 public class BaseInfoStart {
 
 	public static void main(String[] args) {
-		newJettyUtil().createServer("/jetty", 8080).start();
+		newJettyUtil().createServer("/platform", 8080).start();
 	}
 
 }
