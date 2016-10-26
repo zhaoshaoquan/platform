866c15447a43a9b673cc55ec653d544a0f4639ce
zhaoshaoquan <10534986@qq.com>
2016-10-26 21:50:31 +0800

    修正启动时出现空指针异常

--- jetty/src/main/java/com/stone/jetty/JettyServletContainerInitializer.java
+++ jetty/src/main/java/com/stone/jetty/JettyServletContainerInitializer.java
@@ -14,7 +14,7 @@ public class JettyServletContainerInitializer implements ServletContainerInitial
     public void onStartup(Set<Class<?>> webAppInitializerClasses, ServletContext servletContext) throws ServletException{
         String mimeTypes = "text/html,text/plain,text/xml,application/xhtml+xml,text/css,application/javascript,application/json,application/x-javascript,image/svg+xml";
         EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST);
-        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("GzipFilter", new GzipFilter());
+        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("JettyGzipFilter", new GzipFilter());
         characterEncodingFilter.setInitParameter("mimeTypes", mimeTypes);
         characterEncodingFilter.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
     }
